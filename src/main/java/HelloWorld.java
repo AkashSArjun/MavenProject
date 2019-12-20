25b01c76c663a4e2399bcc0ad12d6fbab496d3a9
Akash S Arjun <akashsarjun@icloud.com>
2019-12-20 12:17:43 +0100

    Initial commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,16 @@
+/*
+* Crated by Akash S Arjun
+* Date 12/20/2019
+* */
+
+import org.apache.commons.lang3.StringUtils;
+
+public class HelloWorld {
+
+     public static void main(String [] args)
+     {
+         System.out.println("hellooo akash");
+         System.out.println(StringUtils.capitalize("hello akash s arjun"));
+     }
+
+}
